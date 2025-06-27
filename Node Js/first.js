3ff73dbfd9636cce63cdf3a4c301949bde587adb
AasthaGupta3 <101085025+AasthaGupta3@users.noreply.github.com>
2025-06-27 23:01:21 +0530

    Delete Node Js/first.js

--- Node Js/first.js
+++
@@ -1,8 +0,0 @@
-console.log("aastha is the best.");
-const fs =  require('fs');
-fs.writeFile("output.txt", " aastha can do anything" , (err)=>{
-  if (err) throw console.log(" error occurred");
-  else console.log("file written succesfully");
-  
-})
-
